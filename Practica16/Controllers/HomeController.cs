2f621c4b5a4f6c6070581f8792988b56b36b2dd5
Saurabh Mishra <saurabh@simformsolutions.com>
2023-06-21 20:57:25 +0530

    Practical-16 Completed

---
+++ Practica16/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practica16.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult SimpleMessage()
+        {
+            _logger.LogInformation("Log message in the SimpleMessage() method");
+
+            return Content("Hello World");
+        }
+    }
+}
